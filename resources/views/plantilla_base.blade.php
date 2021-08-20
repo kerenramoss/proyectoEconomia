22ef6b717523283bc6731ddc5bc5d31ad195bae4
kerenramoss <69411341+kerenramoss@users.noreply.github.com>
2021-08-19 22:04:40 -0600

    make it better

--- resources/views/plantilla_base.blade.php
+++ resources/views/plantilla_base.blade.php
@@ -9,7 +9,6 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="{{asset('css/login.css')}}">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@500&display=swap" rel="stylesheet">
     </head>
     <body>
