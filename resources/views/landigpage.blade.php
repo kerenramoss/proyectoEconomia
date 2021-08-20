7c73bbdca38a2f81a03d8d60c9b7689fe4524d53
kerenramoss <69411341+kerenramoss@users.noreply.github.com>
2021-08-19 21:11:48 -0600

    make it better

--- resources/views/landigpage.blade.php
+++ resources/views/landigpage.blade.php
@@ -7,6 +7,7 @@
         <title>catracho El Catracho Viajero</title>
 
         <!-- Fonts -->
+        
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
